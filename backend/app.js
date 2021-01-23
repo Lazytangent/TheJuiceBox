96a49abbc1df6942b98dcc1b7dae4a95435374c8
Lazytangent <pm123633@gmail.com>
2021-01-23 17:05:49 -0600

    Basic setup for AWS S3

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (!isProduction) {
---
+++ backend/awsS3.js
@@ -0,0 +1,93 @@
+const AWS = require('aws-sdk');
+const NAME_OF_BUCKET = "the-juice-box";
+
+const multer = require('multer');
+const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+
+/ --------------------------- Public UPLOAD ------------------------
+
+const singlePublicFileUpload = async (file) => {
+  const { originalname, mimetype, buffer } = await file;
+  const path = require("path");
+  // name of the file in your S3 bucket will be the date in ms plus the extension name
+  const Key = new Date().getTime().toString() + path.extname(originalname);
+  const uploadParams = {
+    Bucket: NAME_OF_BUCKET,
+    Key,
+    Body: buffer,
+    ACL: "public-read",
+  };
+  const result = await s3.upload(uploadParams).promise();
+
+  // save the name of the file in your bucket as the key in your database to retrieve for later
+  return result.Location;
+};
+
+const multiplePublicFileUpload = async (files) => {
+  return await Promise.all(
+    files.map((file) => {
+      return singlePublicFileUpload(file);
+    })
+  );
+};
+
+// --------------------------- Prviate UPLOAD ------------------------
+
+const singlePrivateFileUpload = async (file) => {
+  const { originalname, mimetype, buffer } = await file;
+  const path = require("path");
+  // name of the file in your S3 bucket will be the date in ms plus the extension name
+  const Key = new Date().getTime().toString() + path.extname(originalname);
+  const uploadParams = {
+    Bucket: NAME_OF_BUCKET,
+    Key,
+    Body: buffer,
+  };
+  const result = await s3.upload(uploadParams).promise();
+
+  // save the name of the file in your bucket as the key in your database to retrieve for later
+  return result.Key;
+};
+
+const multiplePrivateFileUpload = async (files) => {
+  return await Promise.all(
+    files.map((file) => {
+      return singlePrivateFileUpload(file);
+    })
+  );
+};
+
+const retrievePrivateFile = (key) => {
+  let fileUrl;
+  if (key) {
+    fileUrl = s3.getSignedUrl("getObject", {
+      Bucket: NAME_OF_BUCKET,
+      Key: key,
+    });
+  }
+  return fileUrl || key;
+};
+
+// --------------------------- Storage ------------------------
+
+const storage = multer.memoryStorage({
+  destination: function (req, file, callback) {
+    callback(null, "");
+  },
+});
+
+const singleMulterUpload = (nameOfKey) =>
+  multer({ storage: storage }).single(nameOfKey);
+const multipleMulterUpload = (nameOfKey) =>
+  multer({ storage: storage }).array(nameOfKey);
+
+module.exports = {
+  s3,
+  singlePublicFileUpload,
+  multiplePublicFileUpload,
+  singlePrivateFileUpload,
+  multiplePrivateFileUpload,
+  retrievePrivateFile,
+  singleMulterUpload,
+  multipleMulterUpload,
+};
