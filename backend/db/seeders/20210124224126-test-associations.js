265b6fb0258de5821d40c96ba37e25babb29f699
Lazytangent <pm123633@gmail.com>
2021-01-24 17:28:39 -0600

    working on associations

--- backend/db/seeders/20210124224126-test-associations.js
+++ backend/db/seeders/20210124224126-test-associations.js
@@ -3,33 +3,11 @@
 const { User, Drink } = require('../models');
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    const demo1 = await User.findByPk(1);
-    const demo2 = await User.findByPk(2);
-    const demo3 = await User.findByPk(3);
-
-    const drink1 = await Drink.findByPk(1);
-    const drink2 = await Drink.findByPk(2);
-    const drink3 = await Drink.findByPk(3);
-
+  up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('DrinkReviews', [
       {
-        userId: demo1.id,
-        drinkId: drink1.id,
-        review: 'That was some good stuff',
-        isCreator: false,
-        liked: true,
-        stars: 3,
-      },
-      {
-        userId: demo2.id,
-        drinkId: drink1.id,
-        isCreator: true,
-        liked: true,
-      },
-      {
-        userId: demo3.id,
-        drinkId: drink1.id,
+        userId: 1,
+        drinkId: 1,
         review: 'That was some good stuff',
         isCreator: false,
         liked: true,
@@ -39,15 +17,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-    return queryInterface.bulkDelete('DrinkReviews', {
-      id: { [Sequelize.Op.in]: ['1', '2', '3'] }
-    }, {});
+    return queryInterface.bulkDelete('DrinkReviews', null, {});
   }
 };
