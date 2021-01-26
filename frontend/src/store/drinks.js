3732a369e65c8d9ef781a99bbedf18250430ced1
Lazytangent <pm123633@gmail.com>
2021-01-25 23:30:54 -0600

    Basic setup for thunks for get and post routes for drinks

--- frontend/src/store/drinks.js
+++ frontend/src/store/drinks.js
@@ -1,23 +1,52 @@
 import { fetch } from './csrf';
 
-const GET_DRINKS = 'drinks/GET_DRINKS';
-const SET_DRINK = 'drinks/MAKE_DRINK';
+const SET_DRINKS = 'drinks/SET_DRINKS';
+const CREATE_DRINK = 'drinks/CREATE_DRINK';
 
-const getDrinks = () => {
+const setDrinks = (drinks) => {
   return {
-    type: GET_DRINKS,
+    type: SET_DRINKS,
+    drinks,
   };
 };
 
-const setDrink = (drink) => {
+const createDrink = (drink) => {
   return {
-    type: SET_DRINK,
+    type: CREATE_DRINK,
     drink,
   };
 };
 
+export const getDrinks = () => async (dispatch) => {
+  const response = await fetch('/api/drinks');
+  if (response.ok) {
+    dispatch(setDrinks(response.data.drinks));
+    return response;
+  }
+};
+
+export const mixDrink = (drink) => async (dispatch) => {
+  const { image, name, description } = drink;
+  const formData = new FormData();
+  formData.append('name', name);
+  formData.append('description', description);
+  formData.append('image', image);
+
+  const response = await fetch('/api/drinks', {
+    method: 'POST',
+    body: formData,
+  });
+
+  dispatch(createDrink(response.data.drink));
+};
+
 const drinksReducer = (state = initialState, action) => {
+  const newState = { ...state };
   switch (action.type) {
+    case SET_DRINKS:
+      return { ...state, action.drinks };
+    case CREATE_DRINK:
+      return { ...state, [action.drink.id]: action.drink };
     default:
       return state;
   }
