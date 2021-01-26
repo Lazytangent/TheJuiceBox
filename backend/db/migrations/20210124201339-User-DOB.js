03960fcaa46c1056f1c3e37fca89bb5b8be7b4cd
Lazytangent <pm123633@gmail.com>
2021-01-25 18:54:12 -0600

    Started more backend form validations

--- backend/db/migrations/20210124201339-User-DOB.js
+++ backend/db/migrations/20210124201339-User-DOB.js
@@ -2,13 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
     return queryInterface.addColumn('Users', 'dateOfBirth', {
       type: Sequelize.DATEONLY,
       allowNull: false,
@@ -16,13 +9,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
     return queryInterface.removeColumn('Users', 'dateOfBirth');
   }
 };
