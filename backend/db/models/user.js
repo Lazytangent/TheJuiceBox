fafd22ffbaa1d2a0c28d32674245760d42d8f09d
Lazytangent <pm123633@gmail.com>
2021-01-27 12:31:23 -0600

    Cleaned up comments

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -101,7 +101,6 @@ module.exports = (sequelize, DataTypes) => {
     return await User.scope('currentUser').findByPk(user.id);
   };
   User.associate = function(models) {
-    // associations can be defined here
     User.belongsToMany(models.Drink, { through: 'DrinkReviews', foreignKey: 'userId', otherKey: 'drinkId' });
     User.belongsToMany(models.Venue, { through: 'CheckIns', foreignKey: 'userId', otherKey: 'venueId' });
     User.hasMany(models.Drink, { foreignKey: 'creatorId' });
