8b90ee240d268621d493ea980bfa59c26f52d3f7
Lazytangent <pm123633@gmail.com>
2021-01-26 18:57:33 -0600

    Very basic styling on Drink Detail page

--- frontend/src/components/DrinkDetails/DrinkDetails.js
+++ frontend/src/components/DrinkDetails/DrinkDetails.js
@@ -19,17 +19,21 @@ const DrinkDetails = () => {
   if (!isLoaded) return null;
 
   return (
-    <div className="tw-p-8">
-      <img src={drink.imageUrl} alt={drink.name} />
-      <h1>Drink No. {drink.id} Details</h1>
-      <h3>The {drink.name}</h3>
-      <p>{drink.description}</p>
-      {user && drink.creatorId === user.id && (
-        <>
-          <button>Edit</button>
-          <button>Delete</button>
-        </>
-      )}
+    <div className="tw-grid-cols-3 tw-grid tw-p-8 tw-flex tw-flex-col tw-items-center">
+      <div className="tw-flex tw-justify-center tw-col-span-1 tw-p-4 tw-max-h-96">
+        <img src={drink.imageUrl} alt={drink.name} />
+      </div>
+      <div className="tw-col-span-2 tw-p-4 tw-flex tw-flex-col">
+        <h1 className="tw-font-serif tw-text-xl tw-font-semibold">Drink No. {drink.id} Details</h1>
+        <h3 className="tw-text-l tw-font-bold">The {drink.name}</h3>
+        <p>{drink.description}</p>
+        {user && drink.creatorId === user.id && (
+          <div className="tw-w-2/4 tw-flex tw-flex-start">
+            <button className="tw-p-1 tw-m-1 tw-border hover:tw-bg-gray-300">Edit</button>
+            <button className="tw-p-1 tw-m-1 tw-border hover:tw-bg-gray-300">Delete</button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
