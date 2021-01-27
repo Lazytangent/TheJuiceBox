57f4f83ddcbddaab05480eba473de6691c269bf6
Lazytangent <pm123633@gmail.com>
2021-01-26 23:09:28 -0600

    Somewhat working delete drink feature

---
+++ frontend/src/components/DrinkDetails/DeleteConfirmation.js
@@ -0,0 +1,29 @@
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import { deleteDrink } from '../../store/drinks';
+
+const DeleteConfirmation = ({ id, setShowDeleteModal }) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const onClick = () => {
+    dispatch(deleteDrink(id)).then(() => history.push('/drinks'));
+  };
+
+  const closeModal = () => {
+    setShowDeleteModal(false);
+  };
+
+  return (
+    <div className="tw-bg-white tw-p-8 tw-rounded">
+      <h2>Are you sure you want to delete this drink?</h2>
+      <div className="tw-p-1 tw-flex tw-justify-around">
+        <button className="tw-p-1 tw-border hover:tw-bg-gray-300" onClick={onClick}>Yes</button>
+        <button className="tw-p-1 tw-border hover:tw-bg-gray-300" onClick={closeModal}>No</button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteConfirmation;
--- frontend/src/components/DrinkDetails/DrinkDetails.js
+++ frontend/src/components/DrinkDetails/DrinkDetails.js
@@ -2,7 +2,9 @@ import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
-import { getDrinks, updateDrink, deleteDrink } from '../../store/drinks';
+import { Modal } from '../../context/Modal';
+import DeleteConfirmation from './DeleteConfirmation';
+import { getDrinks, updateDrink } from '../../store/drinks';
 
 const DrinkDetails = () => {
   const dispatch = useDispatch();
@@ -69,22 +71,29 @@ const DrinkDetails = () => {
   }
 
   return (
-    <div className="tw-grid-cols-3 tw-grid tw-p-8 tw-flex tw-flex-col tw-items-center">
-      <div className="tw-flex tw-justify-center tw-col-span-1 tw-p-4 tw-max-h-96">
-        <img src={drink.imageUrl} alt={drink.name} />
+    <>
+      <div className="tw-grid-cols-3 tw-grid tw-p-8 tw-flex tw-flex-col tw-items-center">
+        <div className="tw-flex tw-justify-center tw-col-span-1 tw-p-4 tw-max-h-96">
+          <img src={drink.imageUrl} alt={drink.name} />
+        </div>
+        <div className="tw-col-span-2 tw-p-4 tw-flex tw-flex-col">
+          <h1 className="tw-font-serif tw-text-xl tw-font-semibold">Drink No. {drink.id} Details</h1>
+          <h3 className="tw-text-l tw-font-bold">The {drink.name}</h3>
+          <p>{drink.description}</p>
+          {user && drink.creatorId === user.id && (
+            <div className="tw-w-2/4 tw-flex tw-flex-start">
+              <button className="tw-p-1 tw-m-1 tw-border hover:tw-bg-gray-300" onClick={editClickHandler}>Edit</button>
+              <button className="tw-p-1 tw-m-1 tw-border hover:tw-bg-gray-300" onClick={deleteClickHandler}>Delete</button>
+            </div>
+          )}
+        </div>
       </div>
-      <div className="tw-col-span-2 tw-p-4 tw-flex tw-flex-col">
-        <h1 className="tw-font-serif tw-text-xl tw-font-semibold">Drink No. {drink.id} Details</h1>
-        <h3 className="tw-text-l tw-font-bold">The {drink.name}</h3>
-        <p>{drink.description}</p>
-        {user && drink.creatorId === user.id && (
-          <div className="tw-w-2/4 tw-flex tw-flex-start">
-            <button className="tw-p-1 tw-m-1 tw-border hover:tw-bg-gray-300" onClick={editClickHandler}>Edit</button>
-            <button className="tw-p-1 tw-m-1 tw-border hover:tw-bg-gray-300" onClick={deleteClickHandler}>Delete</button>
-          </div>
-        )}
-      </div>
-    </div>
+      {showDeleteModal && (
+        <Modal onClose={() => setShowDeleteModal(false)}>
+          <DeleteConfirmation setShowDeleteModal={setShowDeleteModal} id={drink.id} />
+        </Modal>
+      )}
+    </>
   );
 };
 
