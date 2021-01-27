71ddf65458199d3d7fd196fc574c3c990c8b7d7a
Lazytangent <pm123633@gmail.com>
2021-01-26 20:39:31 -0600

    Added Edit and Delete api routes for drinks

--- backend/routes/api/drinks.js
+++ backend/routes/api/drinks.js
@@ -4,7 +4,7 @@ const { check } = require('express-validator');
 
 const { restoreUser } = require('../../utils/auth');
 const { handleValidationErrors } = require('../../utils/validation');
-const { Drink } = require('../../db/models');
+const { Drink, DrinkReview } = require('../../db/models');
 const { singleMulterUpload, singlePublicFileUpload } = require('../../awsS3');
 
 const router = express.Router();
@@ -28,12 +28,11 @@ const validateDrink = [
 router.post('/', singleMulterUpload('image'), restoreUser, validateDrink, asyncHandler(async (req, res) => {
   const { name, description } = req.body;
   const { user } = req;
-  let imageUrl;
+  let imageUrl = null;
   if (req.file) {
     imageUrl = await singlePublicFileUpload(req.file);
-  } else {
-    imageUrl = null;
   }
+
   const drink = await Drink.create({
     name,
     description,
@@ -46,12 +45,44 @@ router.post('/', singleMulterUpload('image'), restoreUser, validateDrink, asyncH
   });
 }));
 
-router.put('/:drinkId', validateDrink, asyncHandler(async (req, res) => {
+router.put('/:drinkId(\\d+)', singleMulterUpload('image'), restoreUser, validateDrink, asyncHandler(async (req, res) => {
+  const drinkId = req.params.drinkId;
+  const id = parseInt(drinkId, 10);
+  const { name, description } = req.body;
+  const { user } = req;
+  let imageUrl = null;
+  if (req.file) {
+    imageUrl = await singlePublicFileUpload(req.file);
+  }
 
+  const drink = await Drink.findByPk(id);
+  await drink.update({
+    name,
+    description,
+    imageUrl: imageUrl || drink.imageUrl,
+  });
+
+  res.json({
+    drink,
+  });
 }));
 
-router.delete('/:drinkId', asyncHandler(async (req, res) => {
+router.delete('/:drinkId(\\d+)', asyncHandler(async (req, res) => {
+  const drinkId = req.params.drinkId;
+  const id = parseInt(drinkId, 10);
+  const drink = await Drink.findByPk(id);
+  const reviews = await DrinkReview.findAll({
+    where: {
+      drinkId: id,
+    },
+  });
 
+  for (let review of reviews) {
+    await review.destory();
+  }
+  await drink.destroy();
+
+  return res.json({ message: 'success' });
 }));
 
 module.exports = router;
