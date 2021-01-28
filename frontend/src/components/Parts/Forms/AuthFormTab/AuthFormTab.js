a43d12f9abcee105a3326c35c6ed35163c6b4912
Lazytangent <pm123633@gmail.com>
2021-01-28 15:59:58 -0600

    Coloring on Auth Form Tabs

--- frontend/src/components/Parts/Forms/AuthFormTab/AuthFormTab.js
+++ frontend/src/components/Parts/Forms/AuthFormTab/AuthFormTab.js
@@ -19,8 +19,8 @@ const AuthFormTab = () => {
 
   return (
     <div className="tw-bg-white tw-border-2 tw-border-black tw-flex">
-      <button disabled={showLoginModal} onClick={LoginTabClickHandler} className="tw-w-2/4 hover:tw-bg-gray-400 disabled:tw-bg-gray-500 disabled:tw-cursor-not-allowed tw-p-1 tw-bg-gray-300 tw-px-3">Log In</button>
-      <button disabled={showRegisterModal} onClick={RegisterTabClickHandler} className="tw-w-2/4 hover:tw-bg-gray-400 disabled:tw-bg-gray-500 disabled:tw-cursor-not-allowed tw-p-1 tw-bg-gray-300 tw-px-3">Register</button>
+      <button disabled={showLoginModal} onClick={LoginTabClickHandler} className="tw-w-2/4 hover:tw-bg-gray disabled:tw-bg-gray disabled:tw-cursor-not-allowed tw-p-1 tw-bg-gray-lightest tw-px-3">Log In</button>
+      <button disabled={showRegisterModal} onClick={RegisterTabClickHandler} className="tw-w-2/4 hover:tw-bg-gray disabled:tw-bg-gray disabled:tw-cursor-not-allowed tw-p-1 tw-bg-gray-lightest tw-px-3">Register</button>
     </div>
   );
 };
