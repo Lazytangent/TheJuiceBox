066f5b225bd3fa9ddfa3ac45f133dd48876e0d84
Lazytangent <pm123633@gmail.com>
2021-01-29 12:33:03 -0600

    Basic Footer completed

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,12 @@
 const Footer = () => {
   return (
-    <div className="tw-flex tw-justify-center">
-      <h1>Created by Peter Mai © 2021</h1>
+    <div className="tw-flex tw-flex-col tw-items-center tw-p-2 tw-bg-black-light tw-justify-center">
+      <div>
+        <h1 className="tw-text-white tw-font-mono tw-text-xl">Created by Peter Mai © 2021</h1>
+      </div>
+      <div>
+        <a href="https://github.com/Lazytangent" className="tw-text-5xl"><i className="hover:tw-shadow hover:tw-text-white fab fa-github-square"></i></a>
+      </div>
     </div>
   );
 };
