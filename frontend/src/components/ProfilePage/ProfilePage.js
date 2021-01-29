4c2b557d1cfe29365810e7dbdf8e725d2a750910
Lazytangent <pm123633@gmail.com>
2021-01-29 13:30:04 -0600

    Layout of Profile page created, now for styling

--- frontend/src/components/ProfilePage/ProfilePage.js
+++ frontend/src/components/ProfilePage/ProfilePage.js
@@ -1,27 +1,50 @@
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 
-import { getUser } from '../../store/users';
+import { getUser } from "../../store/users";
 
 const ProfilePage = () => {
   const dispatch = useDispatch();
   const { userId } = useParams();
 
   const [profile, setProfile] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     dispatch(getUser(userId))
       .then((res) => setProfile(res.data.user))
-      .then((res) => console.log(res.data.user))
+      .then(() => setIsLoaded(true))
       .catch((err) => console.error(err));
   }, [dispatch, userId]);
 
-  console.log(profile);
+  if (!isLoaded) return null;
 
   return (
     <div className="tw-h-screen">
-      <h1>Placeholder for Profile Page</h1>
+      <div className="tw-bg-gray tw-max-w-7xl tw-mx-auto">
+        <h1 className="tw-text-xl tw-font-serif tw-font-semibold">{profile.username}</h1>
+        {profile.Drinks.length > 0 && (
+          <>
+            <h3>Drinks by {profile.username}</h3>
+            <ul>
+              {profile.Drinks.map((drink) => (
+                <li key={drink.id}>{drink.name}</li>
+              ))}
+            </ul>
+          </>
+        )}
+        {profile.DrinkReviews.length > 0 && (
+          <>
+            <h3>{profile.username}'s Reviews of Drinks</h3>
+            <ul>
+              {profile.DrinkReviews.map((review) => (
+                <li key={review.id}>{review.review}</li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
     </div>
   );
 };
