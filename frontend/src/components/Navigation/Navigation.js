a27de60b2a32ec52486206cbb5f0f2f095cb2313
Lazytangent <pm123633@gmail.com>
2021-01-30 12:11:11 -0600

    Some transitions on Profile dropdown

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -82,12 +82,12 @@ const Navigation = ({ isLoaded }) => {
         </ul>
       </nav>
       {sessionUser && (
-        <div className={`tw-absolute tw-right-0 tw-z-10 tw-flex tw-justify-end ${showProfileMenu ? 'tw-transition tw-ease-out tw-duration-100 tw-transform tw-opacity-100 tw-scale-100' : 'tw-transition tw-ease-in tw-duration-75 tw-transform tw-opacity-0 tw-scale-95'}`}>
+        <div className={`tw-absolute tw-right-0 tw-z-10 tw-flex tw-justify-end ${showProfileMenu ? 'tw-transition tw-ease-out tw-duration-100 tw-transform tw-opacity-100 tw-scale-100' : 'tw-transition tw-ease-in tw-duration-100 tw-transform tw-opacity-0 tw-scale-95'}`}>
           <ul className="tw-p-4 tw-text-right tw-bg-green tw-rounded tw-border-2 tw-border-black">
-            <li hidden={!showProfileMenu} className="tw-p-1"><NavLink to={`/users/${sessionUser.id}`} className="hover:tw-underline">{sessionUser.username}</NavLink></li>
-            <li hidden={!showProfileMenu} className="tw-p-1">{sessionUser.email}</li>
+            <li className="tw-p-1"><NavLink to={`/users/${sessionUser.id}`} className="hover:tw-underline">{sessionUser.username}</NavLink></li>
+            <li className="tw-p-1">{sessionUser.email}</li>
             <li>
-              <button hidden={!showProfileMenu} onClick={logout} className="tw-p-1 tw-w-100 tw-px-2 tw-border tw-rounded tw-bg-red hover:tw-bg-red-dark">Log Out</button>
+              <button onClick={logout} className="tw-p-1 tw-w-100 tw-px-2 tw-border tw-rounded tw-bg-red hover:tw-bg-red-dark">Log Out</button>
             </li>
           </ul>
         </div>
