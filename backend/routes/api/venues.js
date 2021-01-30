a6e97e45708a2d04aebc62de89961d10dbcc81c9
Lazytangent <pm123633@gmail.com>
2021-01-29 23:23:32 -0600

    Created basic routes for venues

--- backend/routes/api/venues.js
+++ backend/routes/api/venues.js
@@ -2,9 +2,35 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 const { check } = require('express-validator');
 
+const { restoreUser } = require('../../utils/auth');
 const { handleValidationErrors } = require('../../utils/validation');
-const { Venue } = require('../../db/models');
+const { Venue, CheckIn } = require('../../db/models');
 
 const router = express.Router();
+
+router.get('/', asyncHandler(async (req, res) => {
+  const venues = await Venue.findAll();
+  res.json({ venues });
+}));
+
+router.post('/:venueId(\\d+)/checkIn', restoreUser, asyncHandler(async (req, res) => {
+  const id = parseInt(req.params.venueId, 10);
+  const venue = await Venue.findByPk(id);
+  const { user } = req;
+  // Connect venue to user
+  const checkIn = await CheckIn.create({
+    userId: user.id,
+    venueId: venue.id,
+  });
+  // Not done yet...
+}));
+
+router.put('/:venueId(\\d+)/checkIn/:checkInId(\\d+)', asyncHandler(async (req, res) => {
+  // For updates and creations of reviews of venues
+}));
+
+router.delete('/:venueId(\\d+)/checkIn/:checkInId(\\d+)', asyncHandler(async (req, res) => {
+  // For removing a review of a venue
+}));
 
 module.exports = router;
