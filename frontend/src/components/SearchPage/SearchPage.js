f2625b0d9299bd5fad18f96b2715236344f7ac62
Lazytangent <pm123633@gmail.com>
2021-01-31 10:53:25 -0600

    Modified search results page to have same look as Drinks page

--- frontend/src/components/SearchPage/SearchPage.js
+++ frontend/src/components/SearchPage/SearchPage.js
@@ -26,8 +26,8 @@ const SearchPage = () => {
   }, [setInput]);
 
   return (
-    <div className="tw-bg-gray tw-max-w-7xl tw-mx-auto tw-p-2 tw-min-h-screen">
-      <h2 className="tw-text-3xl tw-font-serif tw-font-semibold tw-text-center">Search Results</h2>
+    <div className="tw-max-w-7xl tw-mx-auto tw-p-2 tw-min-h-screen">
+      <h2 className="tw-text-5xl tw-text-clouds tw-font-serif tw-font-semibold tw-text-center">Search Results</h2>
       {similar.map(drink => (
         <div className="tw-flex tw-justify-center tw-p-4" key={drink.id}>
           <Drink drink={drink} />
