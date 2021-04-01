4ab35b6ea922743557d7fadebff8561d53700dc9
Lazytangent <pm123633@gmail.com>
2021-04-01 13:26:31 -0500

    Remove extra variables

--- backend/routes/api/drinkReviews.js
+++ backend/routes/api/drinkReviews.js
@@ -2,9 +2,8 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 const { check } = require('express-validator');
 
-const { restoreUser } = require('../../utils/auth');
 const { handleValidationErrors } = require('../../utils/validation');
-const { DrinkReview, Drink } = require('../../db/models');
+const { DrinkReview } = require('../../db/models');
 
 const router = express.Router();
 
