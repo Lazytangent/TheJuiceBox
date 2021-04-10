8244de41c978abd00a415bdba82eca6f035ea1d5
Lazytangent <pm123633@gmail.com>
2021-04-10 10:50:16 -0500

    Remove eslint

--- backend/tests/drinkRoutes.test.js
+++ backend/tests/drinkRoutes.test.js
@@ -3,7 +3,32 @@ const app = require('../app');
 const { sequelize, User } = require('../db/models');
 
 describe('Drink routes', () => {
-  describe('GET /api/drinks', () => {
+  beforeAll(async () => {
+    await sequelize.sync({ force: true, logging: false });
+  });
 
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  describe('GET /api/drinks', () => {
+    it('should exist', async () => {
+      await request(app)
+        .get('/api/drinks')
+        .expect(200)
+    });
+
+    it('should return JSON', async () => {
+      await request(app)
+        .get('/api/drinks')
+        .expect('Content-Type', /json/)
+        .expect(200)
+    });
+
+    it('should return all drinks in the database', async () => {
+      const fakeDrink1 = {
+
+      };
+    })
   });
 });
