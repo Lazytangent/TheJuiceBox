843bf1cb51b6ad11e7b44fbc8e18bf8ed85485ba
Lazytangent <pm123633@gmail.com>
2021-04-10 11:36:04 -0500

    Draft tests for GET /api/drinks

--- backend/tests/drinkRoutes.test.js
+++ backend/tests/drinkRoutes.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
+const bcrypt = require('bcryptjs');
+
 const app = require('../app');
-const { sequelize, User } = require('../db/models');
+const { sequelize, User, Drink } = require('../db/models');
 
 describe('Drink routes', () => {
   beforeAll(async () => {
@@ -26,9 +28,36 @@ describe('Drink routes', () => {
     });
 
     it('should return all drinks in the database', async () => {
-      const fakeDrink1 = {
-
+      const fakeUser1 = {
+        username: 'testUser1',
+        email: 'test@aa.io',
+        hashedPassword: bcrypt.hashSync('password'),
+        dateOfBirth: new Date('1990-12-31'),
       };
-    })
+
+      const fakeDrink1 = {
+        name: 'testDrink1',
+        description: 'testDescription',
+        creatorId: 1,
+      };
+      const fakeDrink2 = {
+        name: 'testDrink2',
+        description: 'testDescription',
+        creatorId: 1,
+      };
+
+      await User.create(fakeUser1);
+      await Drink.create(fakeDrink1);
+      await Drink.create(fakeDrink2);
+
+      const res = await request(app)
+        .get('/api/drinks')
+        .expect(200);
+
+      expect(res.body).toEqual(expect.arrayContaining([
+        expect.objectContaining(fakeDrink1),
+        expect.objectContaining(fakeDrink2),
+      ]));
+    });
   });
 });
