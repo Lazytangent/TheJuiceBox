d1f8345f174382aef7ceff5184b1b2377e9047df
Lazytangent <pm123633@gmail.com>
2021-04-10 10:46:38 -0500

    Create drinks routes test file

---
+++ backend/tests/drinkRoutes.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../app');
+const { sequelize, User } = require('../db/models');
+
+describe('Drink routes', () => {
+  describe('GET /api/drinks', () => {
+
+  });
+});
