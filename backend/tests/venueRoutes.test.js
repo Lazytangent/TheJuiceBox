155ac23288b0b93387d49e35759ee0bfc2d551cd
Lazytangent <pm123633@gmail.com>
2021-04-10 19:07:28 -0500

    Draft venue route

--- backend/tests/venueRoutes.test.js
+++ backend/tests/venueRoutes.test.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
 const app = require("../app");
-const { testModelOptions, getCSRFTokens, loginUser } = require('../utils/test-utils');
+const { testUser, testModelOptions, getCSRFTokens, loginUser } = require('../utils/test-utils');
 const { sequelize, Venue } = require('../db/models');
 
 describe("Venue routes", () => {
@@ -18,24 +18,48 @@ describe("Venue routes", () => {
   });
 
   const fakeVenue1 = {
-
+    name: 'Test Venue1',
+    city: 'New York City',
+    state: 'New York',
   };
 
   const fakeVenue2 = {
-
+    name: 'Test Venue2',
+    city: 'San Francisco',
+    state: 'California',
   };
 
   describe("GET /api/venues", () => {
     it("should exist", async () => {
-
+      await request(app)
+        .get('/api/venues')
+        .expect(200)
     });
 
     it("should return JSON", async () => {
-
+      await request(app)
+        .get('/api/venues')
+        .expect(200)
+        .expect('Content-Type', /json/)
     });
 
     it("should return all venues in the database", async () => {
+      await Venue.create(fakeVenue1, testModelOptions());
+      await Venue.create(fakeVenue2, testModelOptions());
 
+      const res = await request(app)
+        .get('/api/venues')
+        .expect(200)
+        .expect('Content-Type', /json/)
+
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          venues: expect.arrayContaining([
+            expect.objectContaining(fakeVenue1),
+            expect.objectContaining(fakeVenue2),
+          ]),
+        }),
+      );
     });
   });
 });
