11c921ef058e0affe902e5515f229b0ca281d040
Lazytangent <pm123633@gmail.com>
2021-05-02 21:34:19 -0500

    Working static method

--- backend/db/models/drink.js
+++ backend/db/models/drink.js
@@ -29,12 +29,14 @@ module.exports = (sequelize, DataTypes) => {
       },
     },
   }, {});
+
   Drink.associate = function(models) {
     Drink.belongsToMany(models.User, { through: models.DrinkReview, foreignKey: 'drinkId', otherKey: 'userId' });
     Drink.belongsToMany(models.Venue, { through: models.VenuesDrink, foreignKey: 'drinkId', otherKey: 'venueId' });
     Drink.belongsTo(models.User, { as: 'Creator', foreignKey: 'creatorId' });
     Drink.hasMany(models.DrinkReview, { as: 'Reviews', foreignKey: 'drinkId', onDelete: 'cascade', hooks: true });
   };
+
   Drink.findAllWithStuff = function() {
     const { DrinkReview, User } = require('./');
     return Drink.findAll({
