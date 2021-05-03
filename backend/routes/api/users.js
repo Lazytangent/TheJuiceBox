5b99c139b557e1b8b59d0fa082e2193d202d9b38
Lazytangent <pm123633@gmail.com>
2021-05-02 22:27:15 -0500

    Convert query to static method

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,7 +3,7 @@ const asyncHandler = require('express-async-handler');
 
 const { validateSignup } = require('../utils/validators.js');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User, Drink, DrinkReview } = require('../../db/models');
+const { User } = require('../../db/models');
 
 router.post('/', validateSignup, asyncHandler(async (req, res) => {
   const { email, password, username, dateOfBirth } = req.body;
@@ -15,20 +15,7 @@ router.post('/', validateSignup, asyncHandler(async (req, res) => {
 
 router.get('/:id(\\d+)', requireAuth, asyncHandler(async (req, res) => {
   const userId = parseInt(req.params.id, 10);
-  const user = await User.findByPk(userId, {
-    include: [
-      {
-        model: Drink,
-      },
-      {
-        model: DrinkReview,
-        include: [
-          { model: User, },
-          { model: Drink, },
-        ],
-      },
-    ]
-  });
+  const user = await User.findUserWithStuff(userId);
 
   res.json({ user });
 }));
