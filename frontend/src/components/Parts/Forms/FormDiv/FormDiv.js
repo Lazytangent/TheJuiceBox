013855ca8219afd6e6085c38e7a1238f5f2afb74
Lazytangent <pm123633@gmail.com>
2021-08-05 18:10:33 -0500

    Formatting

--- frontend/src/components/Parts/Forms/FormDiv/FormDiv.js
+++ frontend/src/components/Parts/Forms/FormDiv/FormDiv.js
@@ -1,42 +1,95 @@
-const FormDiv = ({ col, labelName = "", required, type, value, onChange, placeholder = "" }) => {
+const FormDiv = ({
+  col,
+  labelName = "",
+  required,
+  type,
+  value,
+  onChange,
+  placeholder = "",
+}) => {
   if (col && type === "textarea" && labelName) {
     return (
       <div className="tw-p-4 tw-flex-col tw-m-2 tw-flex tw-justify-between">
         <label className="tw-p-1.5 tw-flex tw-items-center">{labelName}</label>
-        <textarea required={required} rows="3" cols="50" className="tw-p-1.5 tw-ml-1.5 tw-border tw-rounded" value={value} placeholder={placeholder} onChange={onChange}></textarea>
+        <textarea
+          required={required}
+          rows="3"
+          cols="50"
+          className="tw-p-1.5 tw-ml-1.5 tw-border tw-rounded"
+          value={value}
+          placeholder={placeholder}
+          onChange={onChange}
+        ></textarea>
       </div>
     );
   } else if (col && labelName && type !== "textarea") {
     return (
       <div className="tw-p-4 tw-m-2 tw-flex-col tw-flex tw-justify-between">
         <label className="tw-p-1.5 tw-flex tw-items-center">{labelName}</label>
-        <input required={required} className="tw-p-1.5 tw-ml-1.5 tw-border tw-rounded" type={type} value={value} onChange={onChange} placeholder={placeholder} />
+        <input
+          required={required}
+          className="tw-p-1.5 tw-ml-1.5 tw-border tw-rounded"
+          type={type}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+        />
       </div>
     );
   } else if (type === "textarea" && labelName) {
     return (
       <div className="tw-p-4 tw-m-2 tw-flex tw-justify-between">
         <label className="tw-p-1.5 tw-flex tw-items-center">{labelName}</label>
-        <textarea required={required} rows="3" cols="50" className="tw-p-1.5 tw-ml-1.5 tw-border tw-rounded" value={value} placeholder={placeholder} onChange={onChange}></textarea>
+        <textarea
+          required={required}
+          rows="3"
+          cols="50"
+          className="tw-p-1.5 tw-ml-1.5 tw-border tw-rounded"
+          value={value}
+          placeholder={placeholder}
+          onChange={onChange}
+        ></textarea>
       </div>
     );
   } else if (labelName && type !== "textarea") {
     return (
       <div className="tw-p-4 tw-m-2 tw-flex tw-justify-between">
         <label className="tw-p-1.5 tw-flex tw-items-center">{labelName}</label>
-        <input required={required} className="tw-p-1.5 tw-ml-1.5 tw-border tw-rounded" type={type} value={value} onChange={onChange} placeholder={placeholder} />
+        <input
+          required={required}
+          className="tw-p-1.5 tw-ml-1.5 tw-border tw-rounded"
+          type={type}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+        />
       </div>
     );
   } else if (type === "textarea") {
     return (
       <div className="tw-p-4 tw-m-2 tw-flex tw-justify-between">
-        <textarea rows="3" cols="55" required={required} className="tw-p-1.5 tw-ml-1.5 tw-border tw-rounded" value={value} placeholder={placeholder} onChange={onChange}></textarea>
+        <textarea
+          rows="3"
+          cols="55"
+          required={required}
+          className="tw-p-1.5 tw-ml-1.5 tw-border tw-rounded"
+          value={value}
+          placeholder={placeholder}
+          onChange={onChange}
+        ></textarea>
       </div>
     );
   } else {
     return (
       <div className="tw-p-4 tw-m-2 tw-flex tw-justify-between">
-        <input required={required} className="tw-p-1.5 tw-ml-1.5 tw-border tw-rounded" type={type} value={value} onChange={onChange} placeholder={placeholder} />
+        <input
+          required={required}
+          className="tw-p-1.5 tw-ml-1.5 tw-border tw-rounded"
+          type={type}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+        />
       </div>
     );
   }
