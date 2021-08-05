4f4f2044e2437522cf6c38c969e5f11a56877f0c
Lazytangent <pm123633@gmail.com>
2021-08-05 18:12:10 -0500

    Formatting

--- frontend/src/components/SignupFormModal/SignupForm.js
+++ frontend/src/components/SignupFormModal/SignupForm.js
@@ -1,23 +1,24 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 
-import FormDiv from '../Parts/Forms/FormDiv';
-import AuthFormTab from '../Parts/Forms/AuthFormTab';
-import DemoAuth from '../Parts/Forms/DemoAuth';
-import ErrorsDiv from '../Parts/Forms/ErrorsDiv';
-import SubmitBtn from '../Parts/Forms/SubmitBtn';
-import { registerUser } from '../../store/session';
+import { session } from "../../store/selectors";
+import { registerUser } from "../../store/session";
+import FormDiv from "../Parts/Forms/FormDiv";
+import AuthFormTab from "../Parts/Forms/AuthFormTab";
+import DemoAuth from "../Parts/Forms/DemoAuth";
+import ErrorsDiv from "../Parts/Forms/ErrorsDiv";
+import SubmitBtn from "../Parts/Forms/SubmitBtn";
 
-const SignupForm= () => {
+const SignupForm = () => {
   const dispatch = useDispatch();
-  const sessionUser = useSelector(state => state.session.user);
+  const sessionUser = useSelector(session.user());
 
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
   const [errors, setErrors] = useState([]);
 
   if (sessionUser) return <Redirect to="/" />;
@@ -30,26 +31,62 @@ const SignupForm= () => {
         email,
         password,
         dateOfBirth,
-        confirmPassword
+        confirmPassword,
       };
       const res = await dispatch(registerUser(user));
       if (res.data && res.data.errors) setErrors(res.data.errors);
     } else {
-      setErrors((prev) => [...prev, 'Password confirmation field does not match password field.']);
+      setErrors((prev) => [
+        ...prev,
+        "Password confirmation field does not match password field.",
+      ]);
     }
   };
 
   return (
     <>
       <AuthFormTab />
-      <form className="tw-bg-white tw-w-auto tw-m-auto tw-flex tw-flex-col tw-justify-between tw-p-8 tw-border-2 tw-border-black tw-rounded-lg" onSubmit={onSubmit}>
+      <form
+        className="tw-bg-white tw-w-auto tw-m-auto tw-flex tw-flex-col tw-justify-between tw-p-8 tw-border-2 tw-border-black tw-rounded-lg"
+        onSubmit={onSubmit}
+      >
         <h3 className="tw-text-3xl tw-text-center tw-p-1">Register</h3>
         <ErrorsDiv errors={errors} />
-        <FormDiv labelName="Username" required={true} type="text" value={username} onChange={e => setUsername(e.target.value)} />
-        <FormDiv labelName="Email" required={true} type="email" value={email} onChange={e => setEmail(e.target.value)} />
-        <FormDiv labelName="Password" required={true} type="password" value={password} onChange={e => setPassword(e.target.value)} />
-        <FormDiv labelName="Confirm Password" required={true} type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
-        <FormDiv labelName="Date of Birth" required={true} type="date" value={dateOfBirth} onChange={e => setDateOfBirth(e.target.value)} />
+        <FormDiv
+          labelName="Username"
+          required={true}
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <FormDiv
+          labelName="Email"
+          required={true}
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <FormDiv
+          labelName="Password"
+          required={true}
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <FormDiv
+          labelName="Confirm Password"
+          required={true}
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <FormDiv
+          labelName="Date of Birth"
+          required={true}
+          type="date"
+          value={dateOfBirth}
+          onChange={(e) => setDateOfBirth(e.target.value)}
+        />
         <SubmitBtn name="Register" />
         <DemoAuth />
       </form>
