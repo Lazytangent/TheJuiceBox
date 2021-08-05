0b26677608e326b06691683542f49566ffbfb7bb
Lazytangent <pm123633@gmail.com>
2021-08-05 17:48:29 -0500

    Format

--- frontend/src/components/Parts/Forms/DemoAuth/DemoAuth.js
+++ frontend/src/components/Parts/Forms/DemoAuth/DemoAuth.js
@@ -1,6 +1,6 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 
-import { demoLogin } from '../../../../store/session';
+import { demoLogin } from "../../../../store/session";
 
 const DemoAuth = () => {
   const dispatch = useDispatch();
@@ -11,9 +11,15 @@ const DemoAuth = () => {
 
   return (
     <div className="tw-flex tw-justify-center tw-pt-1.5">
-      <button type="button" className="tw-border-2 tw-border-black tw-p-1 tw-rounded tw-bg-yellow hover:tw-bg-yellow-dark tw-text-xl" onClick={loginAsDemo}>Login As Demo</button>
+      <button
+        type="button"
+        className="tw-border-2 tw-border-black tw-p-1 tw-rounded tw-bg-yellow hover:tw-bg-yellow-dark tw-text-xl"
+        onClick={loginAsDemo}
+      >
+        Login As Demo
+      </button>
     </div>
-  )
+  );
 };
 
 export default DemoAuth;
