909212d2524dfb610a46970eaded0a15ba887ce5
Lazytangent <pm123633@gmail.com>
2021-08-05 17:41:29 -0500

    Format

--- frontend/src/components/Parts/Buttons/NavButton/NavButton.js
+++ frontend/src/components/Parts/Buttons/NavButton/NavButton.js
@@ -1,6 +1,11 @@
 const NavButton = ({ onClick, name }) => {
   return (
-    <button className="tw-border-2 tw-p-1 tw-rounded tw-border-black tw-bg-purple hover:tw-bg-purple-dark" onClick={onClick}>{ name }</button>
+    <button
+      className="tw-border-2 tw-p-1 tw-rounded tw-border-black tw-bg-purple hover:tw-bg-purple-dark"
+      onClick={onClick}
+    >
+      {name}
+    </button>
   );
 };
 
