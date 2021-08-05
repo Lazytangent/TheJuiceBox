61dc49c52777e3088cb6ebd9bc4b8665bca69a8d
Lazytangent <pm123633@gmail.com>
2021-08-05 16:17:24 -0500

    Formatting

--- frontend/src/components/Navigation/SessionLinks.js
+++ frontend/src/components/Navigation/SessionLinks.js
@@ -2,8 +2,8 @@ import { useSelector } from "react-redux";
 
 import { session } from "../../store/selectors";
 import ProfileButton from "./ProfileButton";
-import LoginFormModal from '../LoginFormModal';
-import SignupFormModal from '../SignupFormModal';
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
 
 const SessionLinks = ({ showProfileMenu, setShowProfileMenu }) => {
   const sessionUser = useSelector(session.user());
@@ -16,6 +16,7 @@ const SessionLinks = ({ showProfileMenu, setShowProfileMenu }) => {
   if (sessionUser) {
     return <ProfileButton user={sessionUser} openMenu={openMenu} />;
   }
+
   return (
     <li className="tw-flex tw-justify-between">
       <ul className="tw-flex">
