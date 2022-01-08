04acf156d7ad97d86f70ed3dd44125306af1a6f6
Lazytangent <pm123633@gmail.com>
2022-01-08 17:30:14 -0600

    Add a `userId` column to Venues table

---
+++ backend/db/migrations/20220108230205-venues-userId.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Venues', 'userId', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Venues', 'userId');
+  }
+};
