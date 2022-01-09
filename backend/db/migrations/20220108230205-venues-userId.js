379ef4c7483ca71ca6b950f44da314fc3033bd9d
Lazytangent <pm123633@gmail.com>
2022-01-08 20:24:38 -0600

    Formatting

--- backend/db/migrations/20220108230205-venues-userId.js
+++ backend/db/migrations/20220108230205-venues-userId.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('Venues', 'userId', {
+    return queryInterface.addColumn("Venues", "userId", {
       type: Sequelize.INTEGER,
       allowNull: false,
     });
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('Venues', 'userId');
-  }
+    return queryInterface.removeColumn("Venues", "userId");
+  },
 };
